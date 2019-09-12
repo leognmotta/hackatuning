123b023942e80734b686bed732ec9f18c9b18eb5
Leonardo Motta <mail@leomotta.me>
2019-09-12 15:57:35 -0400

    Add unform & Yup validation

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import { Form, Input } from '@rocketseat/unform';
+import { schema } from './validationSchema';
 
 import { StyledForm } from './styles';
 
 export default function SignIn({ test }) {
   return (
-    <StyledForm onSubmit={() => {}}>
+    <StyledForm schema={schema} onSubmit={() => {}}>
       <h1>Sign in</h1>
 
-      <Input name="email" label="email: " type="email" />
-      <Input name="password" label="password: " type="password" />
+      <Input type="email" name="email" label="email: " />
+      <Input type="password" name="password" label="password: " />
 
       <h1>{test}</h1>
 

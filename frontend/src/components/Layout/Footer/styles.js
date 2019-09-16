ce7392a5d1f2a08ae8a2c1fd98fdaa8964ea8fd1
Leonardo Motta <mail@leomotta.me>
2019-09-16 09:52:22 -0400

    Style footer

--- frontend/src/components/Layout/Footer/styles.js
+++ frontend/src/components/Layout/Footer/styles.js
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
 
 export const Container = styled.footer`
-  background-color: #999;
+  background-color: #1437e3;
 
   h1 {
     color: #fff;
     text-align: center;
+  }
+
+  img {
+    display: block;
+    margin: 15px auto;
   }
 `;

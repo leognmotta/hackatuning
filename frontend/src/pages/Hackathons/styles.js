2110155bcd6285463721b73d7054c13d26e0f200
Adam Dias <adam.dias@outlook.com>
2019-09-20 17:54:19 -0300

    update css

--- frontend/src/pages/Hackathons/styles.js
+++ frontend/src/pages/Hackathons/styles.js
@@ -5,8 +5,17 @@ export const Container = styled.div`
     text-align: center;
   }
 
+  margin: 30px auto;
+
   .heading_section {
-    border-bottom: 1px solid black;
+    margin-top: 35px;
+    margin-bottom: 22px;
+    padding-bottom: 17px;
+    border-bottom: 1px solid #bbd7fb;
+    text-align: left;
+    font-weight: bold;
+    font-size: 24px;
+    color: #210d4a;
   }
 
   .toast-background {
@@ -14,21 +23,150 @@ export const Container = styled.div`
   }
   .toast-font-size {
     color: #fff;
+    text-align: left;
+    font-size: 14px;
     font-weight: bold;
   }
   .toast-progress-bar {
     background: red;
   }
   .toast-background_success {
-    background: lightgreen;
+    background: #04a777;
   }
   .toast-progress-bar_success {
-    background: green;
+    background: #333745;
   }
 `;
 
 export const ParticipantContainer = styled.div``;
 
-export const ManageHackathonContainer = styled.div``;
+export const ManageHackathonContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  margin: 0 auto;
+`;
 
-export const Card = styled.div``;
+export const Card = styled.div`
+  width: 25%;
+  padding: 15px;
+
+  @media only screen and (max-width: 995px) {
+    width: 50%;
+  }
+
+  @media only screen and (max-width: 639px) {
+    width: 100%;
+  }
+
+  .subtitle {
+    font-size: 14px;
+    margin-bottom: 5px;
+  }
+
+  .content {
+    background: #ffffff 0% 0% no-repeat padding-box;
+    box-shadow: 0px 1px 20px #0000000d;
+    border-radius: 6px;
+  }
+
+  header {
+    height: 150px;
+    width: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 6px 6px 0 0;
+
+    h2 {
+      color: #fff;
+      font-family: 'Montserrat Alternates', sans-serif;
+      padding: 22px;
+      font-size: 20px;
+      text-align: left;
+    }
+
+    background-image: -webkit-gradient(
+        linear,
+        left top,
+        left bottom,
+        from(rgba(52, 140, 254, 0.6)),
+        to(rgba(20, 55, 227, 0.7))
+      ),
+      url(${props => props.url});
+    background-image: linear-gradient(
+        rgba(52, 140, 254, 0.6),
+        rgba(20, 55, 227, 0.7)
+      ),
+      url(${props => props.url});
+    background-size: cover;
+    background-position: center;
+  }
+
+  .card_content {
+    padding: 20px;
+
+    div {
+      display: flex;
+      margin: 10px 0;
+      align-items: center;
+
+      svg {
+        margin-right: 15px;
+      }
+
+      span {
+        font-weight: 700;
+        font-size: 15px;
+        color: #210d4a;
+      }
+    }
+
+    a {
+      margin-top: 20px;
+      text-align: center;
+      text-decoration: none;
+      font-weight: bold;
+      padding: 10px 15px;
+      display: block;
+      width: 100%;
+      color: #fff;
+      background: transparent
+        linear-gradient(127deg, #348cfe 0%, #163de5 93%, #1437e3 100%) 0% 0%
+        no-repeat padding-box;
+      border-radius: 6px;
+
+      &:visited {
+        color: #fff;
+      }
+    }
+
+    .organized {
+      margin-bottom: 15px;
+
+      span {
+        font-size: 12px !important;
+        font-weight: 400;
+      }
+    }
+
+    .actions {
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+
+      button {
+        font-size: 14px;
+        padding: 2px 10px !important;
+        margin-left: 10px;
+      }
+
+      a {
+        margin-top: -2px;
+        max-width: 55px;
+        font-size: 14px;
+        padding: 10px;
+      }
+    }
+  }
+`;
--- frontend/src/pages/RegisterHackathon/index.js
+++ frontend/src/pages/RegisterHackathon/index.js
@@ -8,7 +8,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import { Container } from './styles';
 
-export default function RegisterHackathon() {
+export default function RegisterHackathon({ history }) {
   const [file, setFile] = useState();
   const [form, setForm] = useState({
     title: '',
@@ -54,6 +54,10 @@ export default function RegisterHackathon() {
         bodyClassName: 'toast-font-size',
         progressClassName: 'toast-progress-bar_success',
       });
+
+      setTimeout(() => {
+        history.push('/hackathons');
+      }, 2000);
     } catch (error) {
       toast(
         error.response.data.fields
@@ -70,28 +74,32 @@ export default function RegisterHackathon() {
 
   return (
     <Container>
-      <h1>Rregister Hackathon</h1>
+      <h1 style={{ textAlign: 'center' }}>Register Hackathon</h1>
 
       <Form onSubmit={handleSubmit}>
-        <label htmlFor="cover">
-          Cover image:
-          <input
-            id="cover"
-            type="file"
-            onChange={e => setFile(e.target.files[0])}
-          />
-        </label>
+        <p className="label" style={{ marginTop: '20px' }}>
+          Cover:
+        </p>
+
+        <input
+          className="file"
+          id="cover"
+          type="file"
+          onChange={e => setFile(e.target.files[0])}
+        />
 
         <Input
           label="Title:"
           type="text"
           value={form.title}
+          placeholder="Title:"
           onChange={e => setForm({ ...form, title: e.target.value })}
         />
 
         <Input
           label="Subtitle:"
           type="text"
+          placeholder="Subtitle:"
           value={form.subtitle}
           onChange={e => setForm({ ...form, subtitle: e.target.value })}
         />
@@ -99,15 +107,22 @@ export default function RegisterHackathon() {
         <TextArea
           label="Description:"
           value={form.description}
+          placeholder="Description:"
+          rows="7"
           onChange={e => setForm({ ...form, description: e.target.value })}
         />
 
-        <label htmlFor="checkbox">
+        <label
+          htmlFor="checkbox"
+          className="label"
+          style={{ marginBottom: '10px' }}
+        >
           Is the event online?
           <input
             id="checkbox"
             type="checkbox"
             value={form.online}
+            style={{ marginLeft: '10px' }}
             onChange={() => setForm({ ...form, online: !form.online })}
           />
         </label>
@@ -126,12 +141,14 @@ export default function RegisterHackathon() {
           type="number"
           value={form.min_participants}
           style={{ width: 100, marginLeft: 11 }}
+          placeholder="Min:"
           onChange={e => setForm({ ...form, min_participants: e.target.value })}
         />
 
         <Input
           label="Max Participants:"
           type="number"
+          placeholder="Max:"
           value={form.max_participants}
           style={{ width: 100, marginLeft: 10 }}
           onChange={e => setForm({ ...form, max_participants: e.target.value })}
@@ -139,71 +156,67 @@ export default function RegisterHackathon() {
 
         <TextArea
           label="Awards:"
+          placeholder="Awards:"
+          rows="4"
           value={form.awards}
           onChange={e => setForm({ ...form, awards: e.target.value })}
         />
 
-        <label htmlFor="event_date">
-          Date:
-          <DatePicker
-            id="event_date"
-            selected={form.event_date}
-            value={form.event_date}
-            showTimeInput
-            onClickOutside
-            shouldCloseOnSelect
-            time
-            onChange={date => setForm({ ...form, event_date: new Date(date) })}
-          />
-        </label>
+        <p className="label">Event beginning:</p>
+        <DatePicker
+          className="input"
+          id="event_date"
+          selected={form.event_date}
+          value={form.event_date}
+          showTimeInput
+          onClickOutside
+          shouldCloseOnSelect
+          time
+          onChange={date => setForm({ ...form, event_date: new Date(date) })}
+        />
 
-        <label htmlFor="deadline_subscription">
-          End subscription date:
-          <DatePicker
-            id="deadline_subscription"
-            selected={form.deadline_subscription}
-            value={form.deadline_subscription}
-            showTimeInput
-            onClickOutside
-            shouldCloseOnSelect
-            time
-            onChange={date =>
-              setForm({ ...form, deadline_subscription: new Date(date) })
-            }
-          />
-        </label>
+        <p className="label">Event ending:</p>
+        <DatePicker
+          className="input"
+          id="event_ending"
+          selected={form.event_ending}
+          value={form.event_ending}
+          showTimeInput
+          onClickOutside
+          shouldCloseOnSelect
+          time
+          onChange={date => setForm({ ...form, event_ending: new Date(date) })}
+        />
 
-        <label htmlFor="deadline_team_creation">
-          End team creation date:
-          <DatePicker
-            id="deadline_team_creation"
-            selected={form.deadline_team_creation}
-            value={form.deadline_team_creation}
-            showTimeInput
-            onClickOutside
-            shouldCloseOnSelect
-            time
-            onChange={date =>
-              setForm({ ...form, deadline_team_creation: new Date(date) })
-            }
-          />
-        </label>
+        <p className="label">End subscription date:</p>
+        <DatePicker
+          className="input"
+          id="deadline_subscription"
+          selected={form.deadline_subscription}
+          value={form.deadline_subscription}
+          showTimeInput
+          onClickOutside
+          shouldCloseOnSelect
+          time
+          onChange={date =>
+            setForm({ ...form, deadline_subscription: new Date(date) })
+          }
+        />
 
-        <label htmlFor="event_ending">
-          End event date:
-          <DatePicker
-            id="event_ending"
-            selected={form.event_ending}
-            value={form.event_ending}
-            showTimeInput
-            onClickOutside
-            shouldCloseOnSelect
-            time
-            onChange={date =>
-              setForm({ ...form, event_ending: new Date(date) })
-            }
-          />
-        </label>
+        <p className="label">End team creation date:</p>
+        <DatePicker
+          className="input"
+          id="deadline_team_creation"
+          selected={form.deadline_team_creation}
+          value={form.deadline_team_creation}
+          showTimeInput
+          onClickOutside
+          shouldCloseOnSelect
+          time
+          onChange={date =>
+            setForm({ ...form, deadline_team_creation: new Date(date) })
+          }
+        />
 
         <Button text="Send" type="submit" />
       </Form>

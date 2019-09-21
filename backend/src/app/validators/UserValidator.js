0bd4e2b815e2b1222c3a9413aee00b626607dd4b
Leonardo Motta <mail@leomotta.me>
2019-09-20 20:02:36 -0400

    Add url min

--- backend/src/app/validators/UserValidator.js
+++ backend/src/app/validators/UserValidator.js
@@ -14,7 +14,11 @@ class UserValidator {
         password: Yup.string()
           .required()
           .min(6),
-        urls: Yup.array().of(Yup.string().url('url not valid')),
+        urls: Yup.array().of(
+          Yup.string()
+            .url('url not valid')
+            .min(5)
+        ),
         roles: Yup.array()
           .of(Yup.number())
           .required(),
@@ -59,7 +63,11 @@ class UserValidator {
         confirmPassword: Yup.string().when('password', (password, field) =>
           password ? field.required().oneOf([Yup.ref('password')]) : field
         ),
-        urls: Yup.array().of(Yup.string().url('url not valid')),
+        urls: Yup.array().of(
+          Yup.string()
+            .url('url not valid')
+            .min(5)
+        ),
         roles: Yup.array().of(Yup.number()),
       });
 

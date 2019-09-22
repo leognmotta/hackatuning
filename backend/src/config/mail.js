32182fe4bbedc046fce181e08b20c3b7d1e6cc29
Adam Dias <adam.dias@outlook.com>
2019-09-22 14:14:56 -0300

    update mail.config for insecure

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,7 +1,7 @@
 export default {
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT,
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,

c8b6809dba6017e192dc27f489d49f8a418bd465
Adam Dias <adam.dias@outlook.com>
2019-09-22 13:39:45 -0300

    update mail.config for secure

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,7 +1,7 @@
 export default {
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT,
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,

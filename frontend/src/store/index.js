8bdb33d5fbbf5eac2c9a260a81c7589a563dedba
Leonardo Motta <mail@leomotta.me>
2019-09-21 22:43:21 -0400

    Remove redux console log

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,9 +5,7 @@ import reducers from './ducks';
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const logger = store => next => action => {
-  console.log('[Middleware] Dispatching', action);
   const result = next(action);
-  console.log('[Middleware] next state', store.getState());
   return result;
 };
 
